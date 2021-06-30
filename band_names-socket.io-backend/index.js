d3874f208802c23102d307454b0e3b7f55072687
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-06-30 20:38:39 +0100

    conservas entre cliente e servidor

--- band_names-socket.io-backend/index.js
+++ band_names-socket.io-backend/index.js
@@ -15,6 +15,12 @@ io.on("connection", (client) => {
   client.on("disconnect", () => {
     console.log("Cliente desconectado");
   });
+
+  client.on("broadcast", (payload) => {
+    console.log("Mensagem!!!" + payload.name);
+
+    io.emit("broadcast", { admin: "Nova mensagem" });
+  });
 });
 
 //Mensajes de SocketS
