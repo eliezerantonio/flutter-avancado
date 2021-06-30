726b6cd36aa295b7808799fb6f551b156db5b9c7
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-06-30 20:24:28 +0100

    conexao com socket

--- band_names-socket.io-backend/index.js
+++ band_names-socket.io-backend/index.js
@@ -1,15 +1,30 @@
 const express = require("express");
-const app = express();
 require("dotenv").config();
-
 const path = require("path");
+//app do express
+const app = express();
+//Node Server configuration
+
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+
+//mesajes scokets
+io.on("connection", (client) => {
+  console.log("Cliente conectado");
+
+  client.on("disconnect", () => {
+    console.log("Cliente desconectado");
+  });
+});
+
+//Mensajes de SocketS
 
 //puth public
 
 const publicPath = path.resolve(__dirname, "public");
 
 app.use(express.static(publicPath));
-app.listen(process.env.PORT, (err) => {
+server.listen(process.env.PORT, (err) => {
   if (err) throw new Error(err);
 
   console.log(`Servidor correndo na porta ` + process.env.PORT);
