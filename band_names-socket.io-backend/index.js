388cb9ea7bcf3350bb0fa273e92833fa2fce76f7
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-06-30 19:57:59 +0100

    publics

--- band_names-socket.io-backend/index.js
+++ band_names-socket.io-backend/index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 
+const path = require("path");
+
+//puth public
+
+const publicPath = path.resolve(__dirname, "public");
+
+app.use(express.static(publicPath));
 app.listen(3000, (err) => {
   if (err) throw new Error(err);
 
