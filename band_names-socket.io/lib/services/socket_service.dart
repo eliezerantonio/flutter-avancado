efcbbba422f32ee01cfa7e810012c5d4246f740d
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-07-01 20:56:32 +0100

    h

--- band_names-socket.io/lib/services/socket_service.dart
+++ band_names-socket.io/lib/services/socket_service.dart
@@ -1,27 +1,27 @@
 import 'package:flutter/material.dart';
 
 import 'package:socket_io_client/socket_io_client.dart' as IO;
+import 'package:socket_io_client/socket_io_client.dart';
 
 enum ServerStatus { Online, Offline, Connecting }
 
 class SocketService extends ChangeNotifier {
   ServerStatus _serverStatus = ServerStatus.Connecting;
-
+  Socket _socket;
   SocketService() {
-    print("eliezer");
     this._initConfig();
   }
 
   _initConfig() {
     try {
-      IO.Socket socket = IO.io('http://localhost:3000');
-      socket.onConnect((_) {
-        print('connect');
-        socket.emit('msg', 'test');
-      });
-      socket.on('event', (data) => print(data));
-      socket.onDisconnect((_) => print('disconnect'));
-      socket.on('fromServer', (_) => print(_));
+      Socket socket = io(
+          'http://localhost:3000',
+          OptionBuilder()
+              .setTransports(['websocket']) // for Flutter or Dart VM
+              .disableAutoConnect() // disable auto-connection
+              .setExtraHeaders({'foo': 'bar'}) // optional
+              .build());
+      socket.connect();
     } catch (e) {
       print(e);
     }
---
+++ hguf/lib/screens/login_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      child: null,
+    ));
+  }
+}
