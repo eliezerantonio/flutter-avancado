ff5d76fe9af062508329d9f99dca3df2af9b0ae8
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-07-01 21:19:40 +0100

    login screen

--- hguf/lib/screens/login_screen.dart
+++ hguf/lib/screens/login_screen.dart
@@ -7,7 +7,32 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
-      child: null,
+      margin: EdgeInsets.all(20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          TextFormField(
+            decoration: InputDecoration(border: OutlineInputBorder()),
+          ),
+          SizedBox(
+            height: 14,
+          ),
+          TextFormField(
+            decoration: InputDecoration(border: OutlineInputBorder()),
+          ),
+          SizedBox(
+            height: 14,
+          ),
+          Container(
+            height: 44,
+            child: RaisedButton(
+              child: Text("Entrar"),
+              onPressed: () {},
+            ),
+          )
+        ],
+      ),
     ));
   }
 }
