db3b487f77c6760b25757e379c59c5552b13e731
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-07-02 00:15:49 +0100

    estudante

---
+++ hguf/lib/model/student.dart
@@ -0,0 +1,10 @@
+class Student {
+  String name;
+  String genre;
+  String courseId;
+
+  Student.fromMap(Map<String, dynamic> map)
+      : name = map["name"],
+        genre = map["genre"],
+        courseId = map["course"];
+}
