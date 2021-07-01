e76108b227c06ee5f751f5ec7b4c97ab1cc1f71e
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-07-01 22:51:13 +0100

    homescreen

---
+++ hguf/lib/screens/home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text(
+          "Home Screen",
+        ),
+      ),
+    );
+  }
+}
--- hguf/lib/screens/login_screen.dart
+++ hguf/lib/screens/login_screen.dart
@@ -5,56 +5,60 @@ import 'package:provider/provider.dart';
 class LoginScreen extends StatelessWidget {
   LoginScreen({Key key}) : super(key: key);
   final userModel = User();
+  final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      margin: EdgeInsets.all(20),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          TextFormField(
-            onSaved: (email) => userModel.email = email,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: "E-mail",
-            ),
+      body: Container(
+        margin: EdgeInsets.all(20),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextFormField(
+                onSaved: (email) => userModel.email = email,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "E-mail",
+                ),
+              ),
+              SizedBox(
+                height: 14,
+              ),
+              TextFormField(
+                obscureText: true,
+                onSaved: (password) => userModel.password = password,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "Password",
+                ),
+              ),
+              SizedBox(height: 14),
+              Consumer<User>(
+                builder: (_, user, __) {
+                  return Column(
+                    children: [
+                      Container(
+                        height: 44,
+                        child: RaisedButton(
+                          child: Text("Entrar"),
+                          onPressed: () {
+                            formKey.currentState.save();
+                            user.login(userModel);
+                          },
+                        ),
+                      ),
+                      if (user.loading) CircularProgressIndicator()
+                    ],
+                  );
+                },
+              )
+            ],
           ),
-          SizedBox(
-            height: 14,
-          ),
-          TextFormField(
-            obscureText: true,
-            onSaved: (password) => userModel.password = password,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: "Password",
-            ),
-          ),
-          SizedBox(
-            height: 14,
-          ),
-          Consumer<User>(
-            builder: (_, user, __) {
-              return Column(
-                children: [
-                  Container(
-                    height: 44,
-                    child: RaisedButton(
-                      child: Text("Entrar"),
-                      onPressed: () {
-                        user.login(userModel);
-                      },
-                    ),
-                  ),
-                  if (user.loading) CircularProgressIndicator()
-                ],
-              );
-            },
-          )
-        ],
+        ),
       ),
-    ));
+    );
   }
 }
