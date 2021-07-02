052900de09a7bc187c4e668ceb2955d18e6009a3
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-07-02 23:12:28 +0100

    updates

--- band_names-socket.io/lib/pages/status_page.dart
+++ band_names-socket.io/lib/pages/status_page.dart
@@ -1,25 +1,38 @@
-import 'package:band_names/services/socket_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'package:band_names/services/socket_service.dart';
+
+
 class StatusPage extends StatelessWidget {
-  const StatusPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final socketService = context.watch<SocketService>();
-    return Scaffold(
-      body: Container(
-        child: Text("ServerStatus: ${socketService.serverStatus}"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.message),
-        onPressed: () {
-        //emitir :
-       //{nome: 'flutter', message:flutter}
 
-        },
+    final socketService = Provider.of<SocketService>(context);
+    
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('ServerStatus: ${ socketService.serverStatus }')
+          ],
+        ),
+     ),
+     floatingActionButton: FloatingActionButton(
+       child: Icon( Icons.message ),
+       onPressed: (){
+        //  TAREA
+        // emitir: emitir-mensaje
+        // { nombre: 'Flutter', mensaje: 'Hola desde Flutter' }
+        socketService.emit('emitir-mensaje', { 
+          'nombre': 'Flutter', 
+          'mensaje':'Hola desde Flutter' 
+        });
+       }
       ),
-    );
+   );
   }
 }
