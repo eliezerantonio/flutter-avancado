d3297b71d0be2a32540aa19bc945ba471c5212b4
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-07-13 00:26:31 +0100

    logo e messenger

--- chat_flutter/lib/main.dart
+++ chat_flutter/lib/main.dart
@@ -4,13 +4,13 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Chat App',
-      initialRoute: 'loading',
+      initialRoute: 'login',
       routes: appRoutes,
     );
   }
--- chat_flutter/lib/pages/login_page.dart
+++ chat_flutter/lib/pages/login_page.dart
@@ -5,8 +5,63 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xfff2f2f2),
+      body: SafeArea(
+        child: Column(children: [
+          _Logo(),
+        ]),
+      ),
+    );
+  }
+}
+
+class _Logo extends StatelessWidget {
+  const _Logo({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      child: null,
+      width: 170,
+      child: Center(
+        child: Column(
+          children: [
+            Image(
+              image: AssetImage('assets/tag-logo.png'),
+            ),
+            Text(
+              "Messenger",
+              style: TextStyle(
+                fontSize: 30,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _Form extends StatefulWidget {
+  _Form({Key key}) : super(key: key);
+
+  @override
+  __FormState createState() => __FormState();
+}
+
+class __FormState extends State<_Form> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          TextField(),
+          TextField(),
+          RaisedButton(
+            onPressed: () {},
+          )
+        ],
+      ),
     );
   }
 }
