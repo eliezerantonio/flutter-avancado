9b4074d4cf8cfa7a46d9dcd76d046286f503bc2b
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-07-15 04:31:27 +0100

    mudnado de local dos respondes

--- chat-backend/controllers/auth_controller.js
+++ chat-backend/controllers/auth_controller.js
@@ -1,15 +1,6 @@
 const { response } = require("express");
-const { validationResult } = require("express-validator");
 
 const criarUsuario = (req, res = response) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({
-      ok: false,
-      errors: errors.mapped(),
-    });
-  }
-
   res.json({
     ok: true,
     msg: "Criar usuario!!!",
---
+++ chat-backend/middlewares/fields-validators.js
@@ -0,0 +1,13 @@
+const fieldsValidator = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      ok: false,
+      errors: errors.mapped(),
+    });
+  }
+};
+
+module.exports = {
+  fieldsValidator,
+};
