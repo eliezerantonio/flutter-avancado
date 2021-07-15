e96f7f6f8affd337ffa23a1802bbc88a4a00fe87
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-07-15 04:26:06 +0100

    validacao

--- chat-backend/controllers/auth_controller.js
+++ chat-backend/controllers/auth_controller.js
@@ -1,8 +1,18 @@
 const { response } = require("express");
+const { validationResult } = require("express-validator");
+
 const criarUsuario = (req, res = response) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      ok: false,
+      errors: errors.mapped(),
+    });
+  }
+
   res.json({
     ok: true,
-    msg: "criar usuario!!!!",
+    msg: "Criar usuario!!!",
   });
 };
 
--- chat-backend/routes/auth_route.js
+++ chat-backend/routes/auth_route.js
@@ -3,10 +3,16 @@
 path: api/login
 
 */
-const { criarUsuario } = require("../controllers/auth_controller");
+
 const { Router } = require("express");
+const { check } = require("express-validator");
+const { criarUsuario } = require("../controllers/auth_controller");
 const router = Router();
 
-router.post("/new", criarUsuario);
+router.post(
+  "/new",
+  [check("nome", "O nome `e obrigatorio").not().isEmpty()],
+  criarUsuario
+);
 
 module.exports = router;
