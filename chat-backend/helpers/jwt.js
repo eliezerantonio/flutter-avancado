adf639b208976fcdf23cf6766a4575f231b939c5
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-07-17 00:27:46 +0100

    criando promise ao gerar toekn

--- chat-backend/helpers/jwt.js
+++ chat-backend/helpers/jwt.js
@@ -1,11 +1,30 @@
 const jwt = require("jsonwebtoken");
 
 const generateJWT = (uid) => {
-  const payload = {
-    uid,
-  };
-    
-    jwt.sign(payload,)
+  return new Promise((resolve, reject) => {
+    const payload = {
+      uid,
+    };
+
+    jwt.sign(
+      payload,
+      process.env.JWT_KEY,
+      {
+        expires: "24h",
+      },
+      (err, token) => {
+        if (err) {
+          // impossivel criar token
+
+          reject("Nao foi possivel criar token de acesso");
+        } else {
+          ///criar token
+            
+            resolve(token)
+        }
+      }
+    );
+  });
 };
 
 module.exports = {
