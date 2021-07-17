9052ce343fd901ce4e09580ef92d3fbb003c1732
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-07-17 03:33:02 +0100

    CupertinoAlertDialog

--- chat_flutter/lib/helpers/show_alert.dart
+++ chat_flutter/lib/helpers/show_alert.dart
@@ -1,21 +1,39 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
 showAlert(BuildContext context, String title, String subtitle) {
   showDialog(
     context: context,
-    builder: (_) => AlertDialog(
-      title: Text(title),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-      content: Text(subtitle),
-      actions: [
-        MaterialButton(
-          elevation: 5,
-          color: Colors.blue,
-          onPressed: () {},
-          child: Text("Kk"),
-        )
-      ],
-    ),
+    builder: (_) => !Platform.isIOS
+        ? AlertDialog(
+            title: Text(title),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            content: Text(subtitle),
+            actions: [
+              MaterialButton(
+                elevation: 5,
+                color: Colors.blue,
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("Ok"),
+              )
+            ],
+          )
+        : CupertinoAlertDialog(
+            title: Text(title),
+            content: Text(subtitle),
+            actions: [
+              CupertinoButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("Ok"),
+              )
+            ],
+          ),
   );
 }
