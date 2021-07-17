53f9b964e2a9682c4c404f6caf4e98b470f03a26
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-07-17 14:28:57 +0100

    register response

--- chat_flutter/lib/services/auth_service.dart
+++ chat_flutter/lib/services/auth_service.dart
@@ -69,6 +69,40 @@ class AuthService with ChangeNotifier {
     return await _storage.write(key: "token", value: token);
   }
 
+  Future<bool> _register(String name,String email, String password) async {
+
+      try {
+      loading = true;
+      final data = {
+        "name":name,
+        "email": email,
+        "password": password,
+      };
+
+      final response = await http.post(
+        "${Environment.apiUrl}/login/new",
+        body: json.encode(data),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      );
+      if (response.statusCode == 200) {
+        final loginResponse = loginResponseFromJson(response.body);
+        this.user = loginResponse.user;
+
+        await this._saveToken(loginResponse.token);
+        return true;
+
+        //TODO:  guardar token no celular
+      } else {
+        return false;
+      }
+    } catch (e) {} finally {
+      loading = false;
+    }
+
+  }
+
   Future _logout(String token) async {
     return await _storage.delete(key: "token");
   }
