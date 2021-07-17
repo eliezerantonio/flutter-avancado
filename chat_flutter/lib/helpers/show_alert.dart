d9d5f47efeb680589f51dbb9aabffe57a3fe8c22
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-07-17 03:38:24 +0100

    ajuste para ios

--- chat_flutter/lib/helpers/show_alert.dart
+++ chat_flutter/lib/helpers/show_alert.dart
@@ -4,30 +4,33 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
 showAlert(BuildContext context, String title, String subtitle) {
-  showDialog(
-    context: context,
-    builder: (_) => !Platform.isIOS
-        ? AlertDialog(
+  !Platform.isIOS
+      ? showDialog(
+          context: context,
+          builder: (_) => AlertDialog(
+                title: Text(title),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                content: Text(subtitle),
+                actions: [
+                  MaterialButton(
+                    elevation: 5,
+                    color: Colors.blue,
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text("Ok"),
+                  )
+                ],
+              ))
+      : showCupertinoDialog(
+          context: context,
+          builder: (_) => CupertinoAlertDialog(
             title: Text(title),
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
             content: Text(subtitle),
             actions: [
-              MaterialButton(
-                elevation: 5,
-                color: Colors.blue,
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: Text("Ok"),
-              )
-            ],
-          )
-        : CupertinoAlertDialog(
-            title: Text(title),
-            content: Text(subtitle),
-            actions: [
-              CupertinoButton(
+              CupertinoDialogAction(
+                isDefaultAction: true,
                 onPressed: () {
                   Navigator.pop(context);
                 },
@@ -35,5 +38,5 @@ showAlert(BuildContext context, String title, String subtitle) {
               )
             ],
           ),
-  );
+        );
 }
