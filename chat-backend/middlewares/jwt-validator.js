635382f9d6f151a057574bc71ea4c7488351803b
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-07-17 01:17:12 +0100

    verificar se o toekn existe

--- chat-backend/middlewares/jwt-validator.js
+++ chat-backend/middlewares/jwt-validator.js
@@ -4,9 +4,24 @@ const validatorJWT = (req, res, next) => {
   //ler token
   const token = req.header("x-token");
 
-  console.log(token);
+  if (token) {
+    return res.status(401).json({
+      ok: false,
+      msg: "Nao existe nenhum toekn",
+    });
+  }
 
-  next();
+  try {
+    //validar token
+    const { uid } = jwt.verify(token, process.env.JWT_KEY);
+    req.uid = uid;
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      ok: false,
+      msg: "Token invalido",
+    });
+  }
 };
 
 module.exports = {
