6d3c1e3140990c967bceb58bbb2726fcd66c550c
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-07-18 03:41:58 +0100

    socket init

---
+++ chat_flutter/lib/services/socket_service.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+
+enum ServerStatus { Online, Offline, Connecting }
+
+class SocketService with ChangeNotifier {
+  ServerStatus _serverStatus = ServerStatus.Connecting;
+  IO.Socket _socket;
+
+  ServerStatus get serverStatus => this._serverStatus;
+
+  IO.Socket get socket => this._socket;
+  Function get emit => this._socket.emit;
+
+  SocketService() {
+    this._initConfig();
+  }
+
+  void _initConfig() {
+    // Dart client
+    this._socket = IO.io('https://flutter-socket-server.herokuapp.com/', {
+      'transports': ['websocket'],
+      'autoConnect': true
+    });
+
+    this._socket.on('connect', (_) {
+      this._serverStatus = ServerStatus.Online;
+
+      print("cliente conectado ");
+      notifyListeners();
+    });
+
+    this._socket.on('disconnect', (_) {
+      this._serverStatus = ServerStatus.Offline;
+      notifyListeners();
+    });
+  }
+}
