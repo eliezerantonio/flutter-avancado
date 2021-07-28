52d051f8ada725c5e7ad7773ae37d1f140aadf2d
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-07-28 12:25:20 +0100

    rotas

--- maps_apps/lib/main.dart
+++ maps_apps/lib/main.dart
@@ -1,22 +1,21 @@
 import 'package:flutter/material.dart';
- 
+import 'package:maps_apps/pages/access_gps_page.dart';
+import 'package:maps_apps/pages/loading_page.dart';
+import 'package:maps_apps/pages/map_page.dart';
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      home: LoadingPage(),
+      routes: {
+        'map': (_) => MapPage(),
+        'loading': (_) => LoadingPage(),
+        'access_gps': (_) => AccessGpsPage(),
+      },
     );
   }
 }
