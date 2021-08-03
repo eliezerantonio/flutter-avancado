c0cfb2bd47c112d0bf0318967ea619edcf5641ef
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-08-03 18:19:53 +0100

    btn follow location

---
+++ maps_apps/lib/widgets/btn_follow_location.dart
@@ -0,0 +1,25 @@
+part of 'widgets.dart';
+
+class BtnFollowLocation extends StatelessWidget {
+  const BtnFollowLocation({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final mapBloc = BlocProvider.of<MapBloc>(context);
+
+    return Container(
+      margin: EdgeInsets.only(bottom: 10),
+      child: CircleAvatar(
+        backgroundColor: Colors.white,
+        maxRadius: 25,
+        child: IconButton(
+          icon: Icon(Icons.more_horiz),
+          color: Colors.black87,
+          onPressed: () {
+            mapBloc.add(OnMarkTraveled());
+          },
+        ),
+      ),
+    );
+  }
+}
