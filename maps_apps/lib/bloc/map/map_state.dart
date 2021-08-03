cb9e85e64cd97729e3d3684d0b973fa5f8fa2056
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-08-03 19:13:31 +0100

    central location

--- maps_apps/lib/bloc/map/map_state.dart
+++ maps_apps/lib/bloc/map/map_state.dart
@@ -4,8 +4,9 @@ part of 'map_bloc.dart';
 class MapState {
   final bool listMap;
   final bool placeTraveled;
-
   final bool followLocation;
+
+  final LatLng centralLocation;
 //Polylines
   final Map<String, Polyline> polylines;
 
@@ -13,6 +14,7 @@ class MapState {
     this.listMap = false,
     this.placeTraveled = true,
     this.followLocation = false,
+    this.centralLocation,
     Map<String, Polyline> polylines,
   }) : this.polylines = polylines ?? new Map();
 
@@ -20,11 +22,13 @@ class MapState {
     bool listMap,
     bool placeTraveled,
     bool followLocation,
+    LatLng centralLocation,
     Map<String, Polyline> polylines,
   }) =>
       MapState(
         listMap: listMap ?? this.listMap,
         polylines: polylines ?? this.polylines,
+        centralLocation: centralLocation ?? this.centralLocation,
         followLocation: followLocation ?? this.followLocation,
         placeTraveled: placeTraveled ?? this.placeTraveled,
       );
