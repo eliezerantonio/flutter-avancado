44d8f57b2ff1d91ddc49f1baa6387008a4465ee4
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-08-03 19:15:26 +0100

    evento move map

--- maps_apps/lib/bloc/map/map_event.dart
+++ maps_apps/lib/bloc/map/map_event.dart
@@ -5,12 +5,17 @@ abstract class MapEvent {}
 
 class OnListMap extends MapEvent {}
 
+class OnMarkTraveled extends MapEvent {}
+
+class OnFollowLocation extends MapEvent {}
+
 class OnLocationUpdate extends MapEvent {
   final LatLng location;
 
   OnLocationUpdate(this.location);
 }
 
-class OnMarkTraveled extends MapEvent {}
-
-class OnFollowLocation extends MapEvent {}
+class OnMoveMap extends MapEvent {
+  final LatLng location;
+  OnMoveMap(this.location);
+}
