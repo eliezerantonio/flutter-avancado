ee5f48b2c23ff98637ea1df1b7c3557feadce2c4
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-08-05 22:22:54 +0100

    componentes para manjear manual

--- maps_apps/lib/widgets/manual_marker.dart
+++ maps_apps/lib/widgets/manual_marker.dart
@@ -5,9 +5,41 @@ class ManualMarker extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(children: [
+    final width = MediaQuery.of(context).size.width;
+    return Stack(
+      children: [
+        //botao regressar
+        Positioned(
+          top: 70,
+          left: 20,
+          child: CircleAvatar(
+            maxRadius: 25,
+            backgroundColor: Colors.white,
+            child: IconButton(
+              icon: Icon(Icons.arrow_back),
+              color: Colors.black87,
+              onPressed: () {
+                //TODO:fazer algo!!!
+              },
+            ),
+          ),
+        ),
 
-      Positioned());
-    ],
+        Center(child: Icon(Icons.location_on, size: 50)),
+
+        //botao confirmar destino
+        Positioned(
+          bottom: 70,
+          left: 40,
+          child: MaterialButton(
+              minWidth: width - 120,
+              child: Text("Confirmar destino"),
+              color: Colors.black,
+              shape: StadiumBorder(),
+              splashColor: Colors.transparent,
+              onPressed: () {}),
+        )
+      ],
+    );
   }
 }
