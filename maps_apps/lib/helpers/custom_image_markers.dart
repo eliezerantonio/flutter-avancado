055fa30d80e7d5abc4bdf9d66857b61772d088b7
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-08-13 03:56:14 +0100

    BitmapDescriptor

---
+++ maps_apps/lib/helpers/custom_image_markers.dart
@@ -0,0 +1,7 @@
+part of 'helpers.dart';
+
+BitmapDescriptor getassetImageMarker(){ 
+
+
+  
+}
--- maps_apps/lib/helpers/helpers.dart
+++ maps_apps/lib/helpers/helpers.dart
@@ -2,5 +2,8 @@ import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart'
+    show BitmapDescriptor;
 part 'navegar_fadein.dart';
 part 'calculating_alert.dart';
+part 'custom_image_markers.dart';
