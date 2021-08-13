0e76a57fd58eaa96ad1b77d9aa167e632545f23a
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-08-13 23:14:45 +0100

    done2

--- maps_apps/lib/main.dart
+++ maps_apps/lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Material App',
-        initialRoute: 'test_marker_page',
+        initialRoute: 'loading',
         routes: {
           'map': (_) => MapPage(),
           'loading': (_) => LoadingPage(),
